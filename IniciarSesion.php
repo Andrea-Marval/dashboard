7fa33ac77b04a6ea3b4f28a1cf20114ff097ac46
Andrea Marval <andreamarva24l@gmail.com>
2023-07-11 21:28:51 -0400

    Add files via upload

--- IniciarSesion.php
+++ IniciarSesion.php
@@ -1,5 +1,5 @@
 <?php
-
+    session_start();
     include('conexion.php');
 
 if (isset($_POST['Usuario'])&& isset($_POST['Clave'])){ 
@@ -22,14 +22,31 @@ if (isset($_POST['Usuario'])&& isset($_POST['Clave'])){
         header("Location: index.php?error=La Clave Es Requerida");
         exit();
     }else{
-        $Clave = md5($Clave);
+       // $Clave = md5($Clave);
 
         $Sql = "SELECT *FROM personal WHERE Usuario = '$Usuario' AND password='$Clave'";
         $result = mysql_query($conexion, $Sql);
 
         if (mysqli_num_rows($result) === 1) {
             $row = mysqli_fetch_assoc($result);
+            if ($row['Usuario'] === $Usuario && $row['Clave'] === $Clave){
+                $_SESSION['Usuario'] = $row['Usuario'];
+                $_SESSION['Nombre_Completo'] = $row['Nombre_Completo'];
+                $_SESSION['Id'] = $row['Id'];
+                header("Location: Inicio.php");
+                exit();
+            }else {
+                header("Location: Index.php?error=El usuario o la clave son incorrectas");
+                exit();
+            }
+
+        }else{
+            header("Location: Index.php?error=El usuario o la clave son incorrectas");
+            exit();
         }
     }
+}else{
+    header("Location: Index.php");
+    exit();
 }
 
