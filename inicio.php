d92276945b9b2e8a247e877469a7eb60b76f11c9
Andrea Marval <andreamarva24l@gmail.com>
2023-07-11 21:29:33 -0400

    inicio.php

---
+++ inicio.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+     integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Inicio</title>
+</head>
+<body>
+    <CENTER>
+    <h1>HOME</h1>
+    </CENTER>
+    <a href= "CerrarSesion.php" class= "btn btn-danger">Cerrar Sesion</a>
+</body>
+</html>
